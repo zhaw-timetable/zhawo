8cebd5c17319c41e9847d1524bfe6860858f7234
vissejul <vissejul@students.zhaw.ch>
2018-09-20 14:29:31 +0200

    Built inital boilerplate. Added webpack, babel, prittier and react.

---
+++ source/frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
