ea28331efe25343f68f4a1e91f5b584f052460bb
bachmdo <bachdomi1@gmail.com>
2018-10-18 15:44:46 +0200

    Added some tests to Flux action creators

--- source/frontend/app/actions/GlobalActions.test.js
+++ source/frontend/app/actions/GlobalActions.test.js
@@ -1,6 +1,25 @@
 import * as globalActions from './GlobalActions';
+import dispatcher from '../dispatcher.js';
 
-it('basic test', () => {
+const NAME = 'justmejulian';
+
+beforeEach(() => {
+  dispatcher.dispatch = jest.fn();
+});
+
+it('import should be defined', () => {
   expect(globalActions).toBeDefined();
+});
+
+it('all actions should be defined', () => {
   expect(globalActions.setName).toBeDefined();
+});
+
+it('setName should dispatch correct type with payload', () => {
+  globalActions.setName(NAME);
+  expect(dispatcher.dispatch).toHaveBeenCalled();
+  expect(dispatcher.dispatch).toHaveBeenCalledWith({
+    type: 'SET_NAME',
+    payload: NAME
+  });
 });
