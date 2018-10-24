a0bc17dfaebf4e25462128ad17139396148694a3
bachmdo <bachdomi1@gmail.com>
2018-10-25 00:09:38 +0200

    Fixed App component import in main.js, extension not needed

--- source/frontend/main.js
+++ source/frontend/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './app/registerServiceWorker';
 
-import App from './app/App.js';
+import App from './app/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
