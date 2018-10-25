c8ccd8bc380abfc030bda5b69715410c3da60aaa
bachmdo <bachdomi1@gmail.com>
2018-10-25 14:36:33 +0200

    Further testing of heroku

--- source/backend/app/index.js
+++ source/backend/app/index.js
@@ -1,5 +1,7 @@
-import app from "./app";
-import config from "./config.json";
+import '@babel/polyfill';
+
+import app from './app';
+import config from './config.json';
 
 app.server.listen(process.env.PORT || config.port, () => {
   console.log(`Started on port ${app.server.address().port}`);
