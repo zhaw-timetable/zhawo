49d8cdb3eb93ea02c8ca0f22a9c1f8a64ce89e44
bachmdo <bachdomi1@gmail.com>
2018-10-25 14:43:38 +0200

    Tried applying port fix

--- source/backend/app/index.js
+++ source/backend/app/index.js
@@ -3,6 +3,11 @@ import '@babel/polyfill';
 import app from './app';
 import config from './config.json';
 
-app.server.listen(process.env.PORT || config.port, () => {
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = config.port;
+}
+
+app.server.listen(port, () => {
   console.log(`Started on port ${app.server.address().port}`);
 });
