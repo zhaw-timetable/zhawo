faa76c370b3514afae115921eab2ea11bb11d3b1
bachmdo <bachdomi1@gmail.com>
2018-10-25 15:27:14 +0200

    Trying to fix with host

--- source/backend/app/index.js
+++ source/backend/app/index.js
@@ -3,6 +3,6 @@ import '@babel/polyfill';
 import app from './app';
 import config from './config.json';
 
-app.server.listen(process.env.PORT || config.port, () => {
+app.server.listen(process.env.PORT || config.port, '0.0.0.0', () => {
   console.log(`Started on port ${app.server.address().port}`);
 });
