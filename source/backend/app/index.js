2c3677222d7bd4b1a21a86b6ccee2e729c48ccd4
bachmdo <bachdomi1@gmail.com>
2018-10-25 15:34:41 +0200

    Forgot to serve the build

--- source/backend/app/index.js
+++ source/backend/app/index.js
@@ -3,6 +3,6 @@ import '@babel/polyfill';
 import app from './app';
 import config from './config.json';
 
-app.server.listen(process.env.PORT || config.port, '0.0.0.0', () => {
+app.server.listen(process.env.PORT || config.port, () => {
   console.log(`Started on port ${app.server.address().port}`);
 });
