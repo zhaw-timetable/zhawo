4d2038c929b78f7203db489712010a4bfe78293e
bachmdo <bachdomi1@gmail.com>
2018-11-01 14:16:24 +0100

    Fixed bug in api routing

--- source/backend/app/api/v1/index.js
+++ source/backend/app/api/v1/index.js
@@ -3,6 +3,6 @@ import schedules from './schedules';
 
 export default ({ config, db }) => {
   let v1 = Router();
-  v1.use('/schedules', timetable({ config, db }));
+  v1.use('/schedules', schedules({ config, db }));
   return v1;
 };
