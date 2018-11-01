44e43028e814a9b3a7fe5889c2e4d94d391c3af6
bachmdo <bachdomi1@gmail.com>
2018-11-01 14:48:27 +0100

    Improved ZhawoAdapter, added getPossibleNames for suggestions #12 #13 #14

--- source/frontend/app/adapters/ZhawoAdapter.js
+++ source/frontend/app/adapters/ZhawoAdapter.js
@@ -1,12 +1,14 @@
+const GET = 'GET';
 const POST = 'POST';
 const HEADERS = { 'Content-Type': 'application/json' };
 let address = 'http://localhost:4000';
 if (process.env.NODE_ENV !== 'development') address = window.location.origin;
 const apiUrl = `${address}/api/v1`;
 
-export function getResource(base, resource, name, startDate) {
+// f.ex. route = students, name = bachmdo2, startDate = date
+export function getScheduleResource(route, name, startDate) {
   return new Promise(async (resolve, reject) => {
-    const url = `${apiUrl}/${base}/${resource}`;
+    const url = `${apiUrl}/schedules/${route}`;
     const body = JSON.stringify({
       name,
       startDate
@@ -16,6 +18,20 @@ export function getResource(base, resource, name, startDate) {
     const config = { method, body, headers };
     const response = await fetch(url, config).catch(err => console.error(err));
     const json = await response.json().catch(err => console.error(err));
+    json ? resolve(json) : reject();
+    console.info(`Fetched from ${url}`);
+  });
+}
+
+// f.ex. route = students
+export function getPossibleNames(route) {
+  return new Promise(async (resolve, reject) => {
+    const method = GET;
+    const headers = HEADERS;
+    const config = { method, headers };
+    const url = `${apiUrl}/schedules/${route}/`;
+    const response = await fetch(url, config).catch(err => console.error(err));
+    const json = await response.json();
     json ? resolve(json) : reject();
     console.info(`Fetched from ${url}`);
   });
