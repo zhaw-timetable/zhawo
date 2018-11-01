26282c9123ee572abf5efa2ac1f37320d372a7aa
bachmdo <bachdomi1@gmail.com>
2018-11-01 15:12:12 +0100

    Updated tests for backend, not complete

--- source/backend/app/adapters/CampusInfoAdapter.test.js
+++ source/backend/app/adapters/CampusInfoAdapter.test.js
@@ -1,31 +1,55 @@
+import request from 'supertest';
+import fetch from 'node-fetch';
+import { format } from 'date-fns';
+
 import * as api from './CampusInfoAdapter';
+
+const FIXED_DATE = new Date();
+const FETCH_RESPONSE = { status: 'ok' };
 
 beforeEach(() => {
   fetch.resetMocks();
-  console.log = jest.fn();
 });
 
-const USERNAME = 'vissejul';
-const STARTDATE = new Date();
-const FETCH_RESPONSE = { foo: 'bar' };
-
-xit('All functions shhould be exported', () => {
-  expect(timetableAdapter).toBeDefined();
-  expect(timetableAdapter.fetchByUsername).toBeDefined();
+it('all functions shhould be exported', () => {
+  expect(api).toBeDefined();
+  expect(api.getScheduleResource).toBeDefined();
+  expect(api.getPossibleNames).toBeDefined();
 });
 
-xit('Mocking of fetch calls should work', async () => {
+it('getScheduleResource should call correct api endpoint', async () => {
   fetch.once(JSON.stringify(FETCH_RESPONSE));
-  const response = await timetableAdapter.fetchByUsername(USERNAME, STARTDATE);
+  const route = 'students';
+  const name = 'foobar';
+  const dateString = format(FIXED_DATE, 'YYYY-MM-DD');
+  await api.getScheduleResource(route, name, FIXED_DATE);
+  expect(fetch).toHaveBeenCalled();
+  expect(fetch.mock.calls[0][0]).toContain(
+    `/schedules/students/foobar?startingAt=${dateString}`
+  );
+});
+
+it('getScheduleResource should resolve with correct response', async () => {
+  fetch.once(JSON.stringify(FETCH_RESPONSE));
+  const route = 'students';
+  const name = 'foobar';
+  const response = await api.getScheduleResource(route, name, FIXED_DATE);
   expect(response).toBeDefined();
   expect(response).toEqual(FETCH_RESPONSE);
 });
 
-xit('fetchByUsername should call correct api endpoint', async () => {
+it('getPossibleNames should call correct api endpoint', async () => {
   fetch.once(JSON.stringify(FETCH_RESPONSE));
-  const response = await timetableAdapter.fetchByUsername(USERNAME, STARTDATE);
+  const route = 'students';
+  await api.getPossibleNames(route);
   expect(fetch).toHaveBeenCalled();
-  expect(fetch.mock.calls[0][0]).toContain('api/timetable/username');
+  expect(fetch.mock.calls[0][0]).toContain(`/schedules/students/`);
+});
+
+it('getPossibleNames should resolve with correct response', async () => {
+  fetch.once(JSON.stringify(FETCH_RESPONSE));
+  const route = 'students';
+  const response = await api.getPossibleNames(route);
   expect(response).toBeDefined();
   expect(response).toEqual(FETCH_RESPONSE);
 });
--- source/backend/app/app.test.js
+++ source/backend/app/app.test.js
@@ -2,8 +2,11 @@ import request from 'supertest';
 import fetch from 'node-fetch';
 import { format } from 'date-fns';
 
-import { version } from '../package.json';
 import app from './app';
+
+const FETCH_RESPONSE = { status: 'ok' };
+const FIXED_DATE = new Date();
+const API = '/api/v1/';
 
 beforeEach(() => {
   fetch.resetMocks();
@@ -13,50 +16,13 @@ it('App should be exported correctly', () => {
   expect(app).toBeDefined();
 });
 
-const STUDENT_NAME = 'somestud';
-const LECTURER_NAME = 'lect';
-const STARTDATE = new Date();
-
-//TODO: adapt tests to new api organization
-
-xit('POST to /api/timetable/username/ should respond correctly', async () => {
-  fetch.once(JSON.stringify({ status: 'ok' }));
+it('post to /schedules/students/ should respond correctly', async () => {
+  fetch.once(JSON.stringify(FETCH_RESPONSE));
+  const url = `${API}/schedules/students`;
+  const body = { name: 'foobar', startDate: FIXED_DATE };
   const response = await request(app)
-    .post('/api/timetable/username')
-    .send({
-      userName: STUDENT_NAME,
-      startDate: STARTDATE
-    });
+    .post(url)
+    .send(body);
   expect(response.body.status).toBe('ok');
   expect(fetch).toHaveBeenCalled();
-});
-
-xit('POST to /api/timetable/username/ should call /students/ endpoint for student', async () => {
-  fetch.once(JSON.stringify({ status: 'ok' }));
-  const response = await request(app)
-    .post('/api/timetable/username')
-    .send({
-      userName: STUDENT_NAME,
-      startDate: STARTDATE
-    });
-  expect(response.body.status).toBe('ok');
-  expect(fetch).toHaveBeenCalled();
-  expect(fetch.mock.calls[0][0]).toContain('/students/');
-  expect(fetch.mock.calls[0][0]).toContain(STUDENT_NAME);
-  expect(fetch.mock.calls[0][0]).toContain(format(STARTDATE, 'YYYY-MM-DD'));
-});
-
-xit('POST to /api/timetable/username/ should call /lecturers/ endpoint for lecturer', async () => {
-  fetch.once(JSON.stringify({ status: 'ok' }));
-  const response = await request(app)
-    .post('/api/timetable/username')
-    .send({
-      userName: LECTURER_NAME,
-      startDate: STARTDATE
-    });
-  expect(response.body.status).toBe('ok');
-  expect(fetch).toHaveBeenCalled();
-  expect(fetch.mock.calls[0][0]).toContain('/lecturers/');
-  expect(fetch.mock.calls[0][0]).toContain(LECTURER_NAME);
-  expect(fetch.mock.calls[0][0]).toContain(format(STARTDATE, 'YYYY-MM-DD'));
 });
