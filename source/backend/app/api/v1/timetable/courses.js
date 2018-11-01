ef1ea280d7ab9983bd2e04bcaf68b3704d3b0d94
bachmdo <bachdomi1@gmail.com>
2018-11-01 13:45:18 +0100

    Added all timetable routes to backend #12 #14 #13

---
+++ source/backend/app/api/v1/timetable/courses.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import * as api from '../../../adapters/CampusInfoAdapter';
+
+export default ({ config, db }) => {
+  let router = Router();
+
+  router.post('/', async (req, res) => {
+    const name = req.body.name;
+    const startDate = req.body.startDate;
+    const resource = await api.getResource(
+      'schedules',
+      'courses',
+      name,
+      startDate
+    );
+    res.json(resource);
+  });
+
+  return router;
+};
---
+++ source/backend/app/api/v1/timetable/lecturers.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import * as api from '../../../adapters/CampusInfoAdapter';
+
+export default ({ config, db }) => {
+  let router = Router();
+
+  router.post('/', async (req, res) => {
+    const name = req.body.name;
+    const startDate = req.body.startDate;
+    const resource = await api.getResource(
+      'schedules',
+      'lecturers',
+      name,
+      startDate
+    );
+    res.json(resource);
+  });
+
+  return router;
+};
