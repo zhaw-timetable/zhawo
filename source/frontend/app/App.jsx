91279804a9e395e39aff997953759193247dee7c
bachmdo <bachdomi1@gmail.com>
2018-11-02 09:40:12 +0100

    Implemented browserhistory to work with React Router

--- source/frontend/app/App.jsx
+++ source/frontend/app/App.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import './assets/sass/main.sass';
 import './assets/font/font.scss';
 import * as globalActions from './actions/GlobalActions';
 import globalStore from './stores/GlobalStore.js';
 
-import AppBar from './containers/AppBar/AppBar.jsx';
+import AppBar from './containers/TestAppBar/TestAppBar';
+import TestSearch from './containers/TestAppBar/TestSearch';
 import Schedule from './containers/Schedule/Schedule.jsx';
-import Nav from './containers/Nav/Nav.jsx';
+// import Nav from './containers/Nav/Nav.jsx';
+import Nav from './containers/TestAppBar/BottomNavigation';
 import NotFound from './containers/NotFound/NotFound.jsx';
 import Login from './containers/Login/Login.jsx';
 import ZHAWO from './containers/ZHAWO/ZHAWO.jsx';
@@ -21,15 +18,13 @@ import VsZHAW from './containers/VsZHAW/VsZHAW.jsx';
 import Menu from './containers/Menu/Menu.jsx';
 import Profile from './containers/Profile/Profile.jsx';
 
+import history from './history';
+
 class App extends Component {
   state = {
     appTitle: 'Timetable',
     username: globalStore.getUsername()
   };
-
-  // componentWillMount() {
-  //   console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV);
-  // }
 
   render() {
     const SecretRoute = ({ component: Component, ...rest }) => (
@@ -52,13 +47,14 @@ class App extends Component {
     );
 
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <SecretRoute exact path="/" component={Schedule} />
           <SecretRoute exact path="/zhawo" component={ZHAWO} />
           <SecretRoute exact path="/vszhaw" component={VsZHAW} />
           <SecretRoute exact path="/menu" component={Menu} />
           <SecretRoute exact path="/profile" component={Profile} />
+          <SecretRoute exact path="/search" component={TestSearch} />
           <SecretRoute component={NotFound} />
         </Switch>
       </Router>
