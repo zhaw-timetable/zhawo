b0d44542a198c221526f099f15b73f9faf6cc64c
bachmdo <bachdomi1@gmail.com>
2018-11-02 18:59:38 +0100

    Added backend express routes for all resources #12 #13 #14

--- source/backend/app/api/v1/schedules/lecturers.js
+++ source/backend/app/api/v1/schedules/lecturers.js
@@ -5,6 +5,13 @@ import * as api from '../../../adapters/CampusInfoAdapter';
 export default ({ config, db }) => {
   let router = Router();
 
+  router.get('/', async (req, res) => {
+    const resource = await api
+      .getPossibleNames('lecturers')
+      .catch(err => console.error(err));
+    res.json(resource);
+  });
+
   router.post('/', async (req, res) => {
     const name = req.body.name;
     const startDate = req.body.startDate;
