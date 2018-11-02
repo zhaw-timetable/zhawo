e4a5ecfd741350805941695a2f9b692dd82b8a35
bachmdo <bachdomi1@gmail.com>
2018-11-02 19:01:06 +0100

    Updated ZhawoAdapter to loop through all resources for input suggestions

--- source/frontend/app/adapters/ZhawoAdapter.js
+++ source/frontend/app/adapters/ZhawoAdapter.js
@@ -43,16 +43,54 @@ export function getScheduleResource(route, name, startDate, rangeAroundDate) {
   });
 }
 
-// f.ex. route = students
-export function getPossibleNames(route) {
+//
+
+export function getPossibleNames() {
   return new Promise(async (resolve, reject) => {
     const method = GET;
     const headers = HEADERS;
     const config = { method, headers };
-    const url = `${apiUrl}/schedules/${route}/`;
-    const response = await fetch(url, config).catch(err => console.error(err));
-    const json = await response.json();
-    json ? resolve(json) : reject();
+    const routes = ['students', 'lecturers', 'classes', 'courses', 'rooms'];
+    let possibleNames;
+    for (let route of routes) {
+      const url = `${apiUrl}/schedules/${route}/`;
+      const response = await fetch(url, config).catch(err =>
+        handleError(err, url)
+      );
+      let possibleNamesChunk = await response
+        .json()
+        .catch(err => handleError(err, url));
+      if (!possibleNamesChunk.status) {
+        // clean up received data, don't know why CampusInfo sends different
+        // formats for each route..
+        switch (route) {
+          // these get sent as arrays
+          case 'students':
+          case 'classes':
+          case 'rooms':
+            possibleNamesChunk[route] = possibleNamesChunk[route].map(
+              value => ({ label: value, type: route })
+            );
+            break;
+          // these as objects with some properties, only need shortName
+          case 'lecturers':
+            possibleNamesChunk[route] = possibleNamesChunk[route].map(
+              value => ({ label: value.shortName, type: route })
+            );
+            break;
+          // these as objects with a DIFFERENT name for the relevant property..
+          case 'courses':
+            possibleNamesChunk[route] = possibleNamesChunk[route].map(
+              value => ({ label: value.name, type: route })
+            );
+            break;
+          default:
+            break;
+        }
+        possibleNames = { ...possibleNames, ...possibleNamesChunk };
+      }
+    }
+    possibleNames ? resolve(possibleNames) : reject({ status: 404 });
   });
 }
 
