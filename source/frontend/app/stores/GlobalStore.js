edcb970a407af5e3cfbaaada5b1cc9ca5a4518ce
vissejul <vissejul@students.zhaw.ch>
2018-11-07 17:27:16 +0100

    moved indexed to store

--- source/frontend/app/stores/GlobalStore.js
+++ source/frontend/app/stores/GlobalStore.js
@@ -15,6 +15,7 @@ class GlobalStore extends EventEmitter {
     this.possibleLoginNames = [];
     this.drawerOpen = false;
     this.getUsernameFromDB();
+    this.getThemeFromDB();
   }
 
   handleActions(action) {
@@ -65,38 +66,74 @@ class GlobalStore extends EventEmitter {
     } else {
       this.theme = 'lightTheme';
     }
+
+    this.setThemeInDB(this.theme);
   }
 
   async getUsernameFromDB() {
     let dbInsance = await idb.open('zhawoDB', 1, upgradeDB =>
-      upgradeDB.createObjectStore('users', { autoIncrement: true })
+      upgradeDB.createObjectStore('info', { keyPath: 'id' })
     );
-    console.log('getUserNameFromDB');
 
-    let tx = dbInsance.transaction('users', 'readonly');
-    let store = tx.objectStore('users');
+    let tx = dbInsance.transaction('info', 'readonly');
+    let store = tx.objectStore('info');
 
     // add, clear, count, delete, get, getAll, getAllKeys, getKey, put
-    let allSavedItems = await store.getAll();
-    console.log(allSavedItems);
+    let user = await store.get('username');
+    console.log(user);
 
-    console.log(allSavedItems[0].name, allSavedItems[0].type);
-    this.currentUser = allSavedItems[0].name;
-    this.currentUserType = allSavedItems[0].type;
+    console.log(user.username, user.type);
+    this.currentUser = user.username;
+    this.currentUserType = user.type;
+
     this.emit('current_user_changed');
+    dbInsance.close();
+  }
 
+  async getThemeFromDB() {
+    let dbInsance = await idb.open('zhawoDB', 1, upgradeDB =>
+      upgradeDB.createObjectStore('info', { keyPath: 'id' })
+    );
+
+    let tx = dbInsance.transaction('info', 'readonly');
+    let store = tx.objectStore('info');
+
+    // add, clear, count, delete, get, getAll, getAllKeys, getKey, put
+    let theme = await store.get('theme');
+    console.log(theme);
+
+    this.theme = theme.theme;
+    this.emit('theme_changed');
+
+    dbInsance.close();
+  }
+
+  // TODO: change so that what you are saving is the key
+
+  async setThemeInDB(theme) {
+    let dbInsance = await idb.open('zhawoDB', 1, upgradeDB =>
+      upgradeDB.createObjectStore('info', { keyPath: 'id' })
+    );
+
+    let tx = dbInsance.transaction('info', 'readwrite');
+    let store = tx.objectStore('info');
+
+    await store.put({ id: 'theme', theme: theme });
+
+    await tx.complete;
+    console.log(theme, ' saved to indexedDB');
     dbInsance.close();
   }
 
   async setCurrentUser(name, type) {
     let dbInsance = await idb.open('zhawoDB', 1, upgradeDB =>
-      upgradeDB.createObjectStore('users', { autoIncrement: true })
+      upgradeDB.createObjectStore('info', { keyPath: 'id' })
     );
 
-    let tx = dbInsance.transaction('users', 'readwrite');
-    let store = tx.objectStore('users');
+    let tx = dbInsance.transaction('info', 'readwrite');
+    let store = tx.objectStore('info');
 
-    await store.put({ name: name, type: type });
+    await store.put({ id: 'username', username: name, type: type });
 
     await tx.complete;
     console.log(name, type, ' saved to indexedDB');
@@ -104,17 +141,17 @@ class GlobalStore extends EventEmitter {
   }
 
   async removeCurrentUser() {
-    console.log('RemovING user from indexedDB');
+    console.log('Removing user from indexedDB');
     let dbInsance = await idb.open('zhawoDB', 1, upgradeDB =>
-      upgradeDB.createObjectStore('users', { autoIncrement: true })
+      upgradeDB.createObjectStore('info', { autoIncrement: false })
     );
 
-    let tx = dbInsance.transaction('users', 'readwrite');
-    let store = tx.objectStore('users');
+    let tx = dbInsance.transaction('info', 'readwrite');
+    let store = tx.objectStore('info');
 
     let allSavedItems = await store.getAllKeys();
 
-    await store.delete(allSavedItems[0]);
+    await store.delete('username');
 
     await tx.complete;
     console.log('Removed user from indexedDB');
