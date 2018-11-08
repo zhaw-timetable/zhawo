0d8359890d04f2bd1af6633ce6e7f454df8773e7
bachmdo <bachdomi1@gmail.com>
2018-11-08 15:39:46 +0100

    Cleaned up ScheduleSearch auto suggestion

--- source/frontend/app/containers/AppBarContainer/components/ScheduleSearch/ScheduleSearch.jsx
+++ source/frontend/app/containers/AppBarContainer/components/ScheduleSearch/ScheduleSearch.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import './ScheduleSearch.sass';
 
 import globalStore from '../../../../stores/GlobalStore';
 import * as globalActions from '../../../../actions/GlobalActions';
@@ -6,50 +7,18 @@ import * as globalActions from '../../../../actions/GlobalActions';
 import scheduleStore from '../../../../stores/ScheduleStore';
 import * as scheduleActions from '../../../../actions/ScheduleActions';
 
-import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
 import match from 'autosuggest-highlight/match';
 import parse from 'autosuggest-highlight/parse';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-
-//TODO: clean this up!
-
-// const styles = theme => ({
-//   container: {
-//     flexGrow: 1,
-//     position: 'relative',
-//     height: 250
-//   },
-//   suggestionsContainerOpen: {
-//     position: 'absolute',
-//     zIndex: 1,
-//     marginTop: theme.spacing.unit,
-//     left: 0,
-//     right: 0
-//   },
-//   suggestion: {
-//     display: 'block'
-//   },
-//   suggestionsList: {
-//     margin: 0,
-//     padding: 0,
-//     listStyleType: 'none'
-//   }
-// });
 
 class ScheduleSearch extends Component {
   state = {
     value: '',
     suggestions: [],
-    locked: true,
     possibleNames: globalStore.possibleNames
   };
 
@@ -84,21 +53,14 @@ class ScheduleSearch extends Component {
 
   handleChange = (event, { newValue }) => {
     this.setState({
-      value: newValue,
-      locked: true
+      value: newValue
     });
-  };
-
-  setCurrentUser = () => {
-    // Sets globalStore currentUser to value of input
-    globalActions.setCurrentUser(this.state.value);
   };
 
   getSuggestions = value => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
     let count = 0;
-
     return inputLength === 0
       ? []
       : this.state.possibleNames.filter(suggestion => {
@@ -109,59 +71,37 @@ class ScheduleSearch extends Component {
           if (keep) {
             count += 1;
           }
-
           return keep;
         });
   };
 
   getSuggestionValue = suggestion => {
-    // this.setState({
-    //   suggestionValue: suggestion,
-    //   locked: false
-    // });
-    // console.log(suggestion.type, suggestion.label, scheduleStore.displayDay);
+    return suggestion.label;
+  };
+
+  onSuggestionSelected = (event, { suggestion }) => {
+    event.preventDefault();
+    this.props.handleClose();
     scheduleActions.getSchedule(
       suggestion.type,
       suggestion.label,
       scheduleStore.displayDay
     );
-    this.props.handleClose();
-    return suggestion.label;
-  };
-
-  handleEnter = e => {
-    // if (!this.state.locked) {
-    //   scheduleActions.getSchedule(
-    //     'students',
-    //     this.state.value,
-    //     scheduleStore.di
-    //   );
-    // }
-    // const currentDate = new Date();
-    // scheduleActions.getSchedule('students', value, currentDate);
-    console.log(this.state.value);
   };
 
   render() {
     const { handleClose } = this.props;
-
-    // theme={{
-    //   container: classes.container,
-    //   suggestionsContainerOpen: classes.suggestionsContainerOpen,
-    //   suggestionsList: classes.suggestionsList,
-    //   suggestion: classes.suggestion
-    // }}
-
     return (
       <DialogContent className="ScheduleSearch">
         <Autosuggest
-          renderInputComponent={renderInput}
+          renderInputComponent={Input}
           suggestions={this.state.suggestions}
           onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
           onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
-          renderSuggestionsContainer={renderSuggestionsContainer}
+          renderSuggestionsContainer={SuggestionsContainer}
+          renderSuggestion={Suggestion}
           getSuggestionValue={this.getSuggestionValue}
-          renderSuggestion={renderSuggestion}
+          onSuggestionSelected={this.onSuggestionSelected}
           inputProps={{
             placeholder: 'Nach Kürzel suchen',
             value: this.state.value,
@@ -173,16 +113,10 @@ class ScheduleSearch extends Component {
   }
 }
 
-// IntegrationAutosuggest.propTypes = {
-//   classes: PropTypes.object.isRequired
-// };
-
-// export default withStyles(styles)(IntegrationAutosuggest);
 export default ScheduleSearch;
 
-function renderInput(inputProps) {
+const Input = inputProps => {
   const { ref, ...other } = inputProps;
-
   return (
     <TextField
       fullWidth
@@ -192,12 +126,11 @@ function renderInput(inputProps) {
       }}
     />
   );
-}
+};
 
-function renderSuggestion(suggestion, { query, isHighlighted }) {
+const Suggestion = (suggestion, { query, isHighlighted }) => {
   const matches = match(suggestion.label, query);
   const parts = parse(suggestion.label, matches);
-
   return (
     <MenuItem selected={isHighlighted} component="div">
       <div>
@@ -215,14 +148,13 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
       </div>
     </MenuItem>
   );
-}
+};
 
-function renderSuggestionsContainer(options) {
+const SuggestionsContainer = options => {
   const { containerProps, children } = options;
-
   return (
     <Paper {...containerProps} square>
       {children}
     </Paper>
   );
-}
+};
