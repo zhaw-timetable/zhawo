d6439703a48ba7c209b28ff1ab1bcca4cca1747a
vissejul <vissejul@students.zhaw.ch>
2018-11-09 17:59:07 +0100

    moved LessonContainer to own container LessonDay

--- source/frontend/app/containers/ScheduleContainer/ScheduleContainer.jsx
+++ source/frontend/app/containers/ScheduleContainer/ScheduleContainer.jsx
@@ -1,5 +1,4 @@
-import React, { Component, Fragment } from 'react';
-import { format } from 'date-fns';
+import React, { Component } from 'react';
 import './ScheduleContainer.sass';
 
 import globalStore from '../../stores/GlobalStore';
@@ -10,11 +9,10 @@ import * as scheduleActions from '../../actions/ScheduleActions';
 
 import NavigationWeek from './components/NavigationWeek/NavigationWeek';
 import NavigationMonth from './components/NavigationMonth/NavigationMonth';
+import LessonDay from './components/LessonDay/LessonDay';
 
 class Schedule extends Component {
   state = {
-    slots: scheduleStore.slots,
-    scheduleForDisplayDay: scheduleStore.scheduleForDisplayDay,
     isOpen: false
   };
 
@@ -28,20 +26,11 @@ class Schedule extends Component {
     }
   }
 
-  componentWillMount() {
-    scheduleStore.on('schedule_changed', this.refreshSchedule);
-  }
+  // Bind change listener
+  componentWillMount() {}
 
-  componentWillUnmount() {
-    scheduleStore.removeListener('schedule_changed', this.refreshSchedule);
-  }
-
-  refreshSchedule = () => {
-    this.setState({
-      slots: scheduleStore.slots,
-      scheduleForDisplayDay: scheduleStore.scheduleForDisplayDay
-    });
-  };
+  // Unbind change listener
+  componentWillUnmount() {}
 
   toggleMonthView = () => {
     this.setState({
@@ -55,42 +44,7 @@ class Schedule extends Component {
         {!this.state.isOpen && <NavigationWeek />}
         {this.state.isOpen && <NavigationMonth />}
         <div id="Gripper" onClick={this.toggleMonthView} />
-        <div className="LessonContainer">
-          {this.state.slots &&
-            this.state.slots.map(slot => (
-              <Fragment key={format(slot.startTime, 'HH:mm')}>
-                <div className="SlotTime">
-                  <div className="SlotStartTime">
-                    {format(slot.startTime, 'HH:mm')}
-                  </div>
-                  <div className="SlotEndTime">
-                    {format(slot.endTime, 'HH:mm')}
-                  </div>
-                </div>
-              </Fragment>
-            ))}
-          {this.state.scheduleForDisplayDay &&
-            this.state.scheduleForDisplayDay.events.map(event => (
-              <Fragment
-                key={format(event.startTime, 'HH:mm').concat(event.name)}
-              >
-                <div
-                  className="Event"
-                  style={{
-                    gridRowStart: event.startSlot + 1,
-                    gridRowEnd: event.endSlot + 1
-                  }}
-                >
-                  <div className="EventInfo">{event.name}</div>
-                  <div className="EventRoom">
-                    {event.eventRealizations[0] &&
-                      event.eventRealizations[0].room &&
-                      event.eventRealizations[0].room.name}
-                  </div>
-                </div>
-              </Fragment>
-            ))}
-        </div>
+        <LessonDay />
       </div>
     );
   }
