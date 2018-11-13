0a00c7118a505c4cc18f53023f34a0a26d3519da
bachmdo <bachdomi1@gmail.com>
2018-11-13 22:11:31 +0100

    Added conversion of schedule by CampusInfo into a better format Will make many things easier, but a lot of effort to convert everything now. But like this we can: - Dynamically load new chunks (weeks) when needed - Check if something already exists - See how many events are in a day, week, slot (and use this to display dot indicators below navigation, render multiple events at the same time etc)

--- source/frontend/app/adapters/ZhawoAdapter.js
+++ source/frontend/app/adapters/ZhawoAdapter.js
@@ -1,4 +1,12 @@
-import { format, startOfWeek, subWeeks, addWeeks } from 'date-fns';
+import {
+  format,
+  startOfWeek,
+  subWeeks,
+  addWeeks,
+  getDay,
+  getHours,
+  getMinutes
+} from 'date-fns';
 
 const GET = 'GET';
 const POST = 'POST';
@@ -39,11 +47,57 @@ export function getScheduleResource(route, name, startDate, rangeAroundDate) {
           : null;
       }
     }
-    schedule ? resolve(schedule) : reject({ status: 404 });
+    schedule ? resolve(convertSchedule(schedule)) : reject({ status: 404 });
   });
 }
 
-//
+// Is needed to make rendering of multiple events in the same slot
+// possible, also helps with coordinating which schedules to fetch from the api
+// and allows for nice event indicators in Week Navigation and Month
+
+// Receives schedule for exactly one week
+
+function convertSchedule(schedule) {
+  // Initialize converted schedule with weeks as empty object
+  const superiorSchedule = { weeks: {} };
+  // Weeks will be added by the shortened date of the weeks Monday
+  const weekKey = format(schedule.days[0].date, 'YYYY-MM-DD');
+  // Add the currently received week with its identifier/key
+  superiorSchedule.weeks[weekKey] = {};
+  // Loop through all days of the received week
+  schedule.days.forEach(day => {
+    // Except if it's a useless Sunday
+    if (!getDay(day.date) == 0) {
+      // Days within a week will again be identified by their shortened date
+      const dayKey = format(day.date, 'YYYY-MM-DD');
+      // Events will be added to slots dynamically, to make it easier, initialize array
+      day.slots.forEach(slot => {
+        slot.events = [];
+        slot.longestEvent = 0;
+      });
+      // Loop through events and find the correct slot in the array
+      day.events.forEach(event => {
+        // Returns index of slot when hours and minutes of events slot match
+        const index = day.slots.findIndex(slot => {
+          return (
+            getHours(slot.startTime) === getHours(event.slots[0].startTime) &&
+            getMinutes(slot.startTime) === getMinutes(event.slots[0].startTime)
+          );
+        });
+        // Expand events by the current event in the loop
+        day.slots[index].events = [...day.slots[index].events, event];
+        if (event.slots.length > day.slots[index].longestEvent)
+          day.slots[index].longestEvent = event.slots.length;
+      });
+      // Initialize day with dayKey
+      superiorSchedule.weeks[weekKey][dayKey] = {};
+      // Add reference to day.slots in superiorSchedule
+      superiorSchedule.weeks[weekKey][dayKey].slots = day.slots;
+    }
+  });
+  console.log(superiorSchedule);
+  return superiorSchedule;
+}
 
 export function getPossibleNames() {
   return new Promise(async (resolve, reject) => {
