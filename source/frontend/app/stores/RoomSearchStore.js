81e465a111d3a7bc9ad5cd3a9f2df4df58ca74e7
vissejul <vissejul@students.zhaw.ch>
2018-11-16 13:43:56 +0100

    added todo

--- source/frontend/app/stores/RoomSearchStore.js
+++ source/frontend/app/stores/RoomSearchStore.js
@@ -14,6 +14,8 @@ class RoomSearchStore extends EventEmitter {
   async handleActions(action) {
     switch (action.type) {
       case 'GET_FREEROOMJSON':
+        // TODO: save to IDB and then check if there first
+        //       save current time to IDB too and check if too old
         this.freeRooms = await api.getFreeRoomsJson().catch(err => {
           console.error(err);
         });
