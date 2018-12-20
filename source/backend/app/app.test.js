7aa47abd010d1490757e55504d345c52739c1170
bachmdo <bachdomi1@gmail.com>
2018-12-20 11:54:40 +0100

    Added more tests to app in backend

--- source/backend/app/app.test.js
+++ source/backend/app/app.test.js
@@ -4,6 +4,10 @@ import fetch from 'node-fetch';
 import app from './app';
 
 const FETCH_RESPONSE = { status: 'ok' };
+const FETCH_RESPONSE_FACILITIES = { gastronomicFacilities: FETCH_RESPONSE };
+const FETCH_RESPONSE_MENSA = {
+  menuPlans: [{ gastronomicFacilityIds: [1, 2] }]
+};
 const FIXED_DATE = new Date();
 const API = '/api/v1/';
 
@@ -52,5 +56,21 @@ it('GET to /schedules/courses/:name should respond correctly', async () => {
   const url = `${API}/schedules/courses/foobar?startDate=${FIXED_DATE}`;
   const response = await request(app).get(url);
   expect(response.body.status).toBe('ok');
+  expect(fetch).toHaveBeenCalled();
+});
+
+it('GET to /mensa/ should respond correctly', async () => {
+  fetch.once(JSON.stringify(FETCH_RESPONSE_FACILITIES));
+  const url = `${API}/mensa/`;
+  const response = await request(app).get(url);
+  expect(response.body.status).toBe('ok');
+  expect(fetch).toHaveBeenCalled();
+});
+
+it('GET to /mensa/menus/:facilityId should respond correctly', async () => {
+  fetch.once(JSON.stringify(FETCH_RESPONSE_MENSA));
+  const url = `${API}/mensa/menus/1?startDate=${FIXED_DATE}`;
+  const response = await request(app).get(url);
+  expect(response.body).toEqual([{ gastronomicFacilityIds: [1, 2] }]);
   expect(fetch).toHaveBeenCalled();
 });
