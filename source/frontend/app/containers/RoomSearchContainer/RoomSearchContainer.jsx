33937970b4280cb382c5885391ee49d6b9521196
vissejul <vissejul@students.zhaw.ch>
2019-03-22 17:12:16 +0100

    logic to store

--- source/frontend/app/containers/RoomSearchContainer/RoomSearchContainer.jsx
+++ source/frontend/app/containers/RoomSearchContainer/RoomSearchContainer.jsx
@@ -7,32 +7,6 @@ import roomSearchStore from '../../stores/RoomSearchStore';
 import * as roomSearchActions from '../../actions/RoomSearchActions';
 import scheduleStore from '../../stores/ScheduleStore';
 
-import SOE from '../../assets/img/FloorPlans/SOE';
-
-import TB2 from '../../assets/img/FloorPlans/TB/TB2';
-import TB3 from '../../assets/img/FloorPlans/TB/TB3';
-import TB4 from '../../assets/img/FloorPlans/TB/TB4';
-import TB5 from '../../assets/img/FloorPlans/TB/TB5';
-import TB6 from '../../assets/img/FloorPlans/TB/TB6';
-
-import TE2 from '../../assets/img/FloorPlans/TE/TE2';
-import TE3 from '../../assets/img/FloorPlans/TE/TE3';
-import TE4 from '../../assets/img/FloorPlans/TE/TE4';
-import TE5 from '../../assets/img/FloorPlans/TE/TE5';
-import TE6 from '../../assets/img/FloorPlans/TE/TE6';
-
-import TL2 from '../../assets/img/FloorPlans/TL/TL2';
-import TL3 from '../../assets/img/FloorPlans/TL/TL3';
-import TL4 from '../../assets/img/FloorPlans/TL/TL4';
-
-import TH2 from '../../assets/img/FloorPlans/TH/TH2';
-import TH3 from '../../assets/img/FloorPlans/TH/TH3';
-import TH4 from '../../assets/img/FloorPlans/TH/TH4';
-import TH5 from '../../assets/img/FloorPlans/TH/TH5';
-
-import TP2 from '../../assets/img/FloorPlans/TP/TP2';
-import TP4 from '../../assets/img/FloorPlans/TP/TP4';
-
 import AppBarContainer from '../AppBarContainer/AppBarContainer';
 
 import Select from '@material-ui/core/Select';
@@ -42,34 +16,8 @@ import ListItem from '@material-ui/core/ListItem';
 
 class RoomSearchContainer extends Component {
   state = {
-    floor: 'SOE',
-    floors: {
-      SOE: SOE,
-
-      TB2: TB2,
-      TB3: TB3,
-      TB4: TB4,
-      TB5: TB5,
-      TB6: TB6,
-
-      TE2: TE2,
-      TE3: TE3,
-      TE4: TE4,
-      TE5: TE5,
-      TE6: TE6,
-
-      TH2: TH2,
-      TH3: TH3,
-      TH4: TH4,
-      TH5: TH5,
-
-      TL2: TL2,
-      TL3: TL3,
-      TL4: TL4,
-
-      TP2: TP2,
-      TP4: TP4
-    },
+    floor: roomSearchStore.currentFloor,
+    floors: roomSearchStore.floors,
     currentFloors: [],
     freeRooms: null,
     timeSlots: scheduleStore.slots,
@@ -82,10 +30,12 @@ class RoomSearchContainer extends Component {
 
   componentWillMount() {
     roomSearchStore.on('got_currentFreeRooms', this.setFreeRooms);
+    roomSearchStore.on('newFloor', this.setFloor);
   }
 
   componentWillUnmount() {
     roomSearchStore.removeListener('got_currentFreeRooms', this.setFreeRooms);
+    roomSearchStore.removeListener('newFloor', this.setFloor);
   }
 
   setFreeRooms = () => {
@@ -95,40 +45,34 @@ class RoomSearchContainer extends Component {
     });
   };
 
+  setFloor = () => {
+    console.log('roomSearchStore.currentFloor: ', roomSearchStore.currentFloor);
+    console.log(
+      'roomSearchStore.currentFloors: ',
+      roomSearchStore.currentFloors
+    );
+    console.log('this.state.floors: ', this.state.floors);
+
+    this.setState({
+      floor: roomSearchStore.currentFloor,
+      currentFloors: roomSearchStore.currentFloors
+    });
+  };
+
   handleChange = event => {
     roomSearchActions.getFreeRoomsByTime(event.target.value);
   };
 
   handleClick = event => {
-    // Todo : move to store
-
     console.log('Click happened, should change to TB2');
-    console.log(this.state.floor);
     console.log(event.target.id);
-    let nextFloor = event.target.id;
-    let tempFloors = [];
-
-    if (nextFloor != 'SOE') {
-      let building = nextFloor.substring(0, 2);
-      console.log('building: ', building);
-
-      for (let tempFloor in this.state.floors) {
-        if (tempFloor.substring(0, 2) === building) {
-          console.log(tempFloor);
-          tempFloors.push(tempFloor);
-        }
-      }
-    }
-
-    this.setState({
-      floor: nextFloor,
-      currentFloors: tempFloors
-    });
+    roomSearchActions.changeFloor(event.target.id);
   };
 
   render() {
     const isThereData = this.state.freeRooms !== null;
     const Room = this.state.floors[this.state.floor];
+
     return (
       <Fragment>
         <AppBarContainer />
--- source/frontend/app/stores/RoomSearchStore.js
+++ source/frontend/app/stores/RoomSearchStore.js
@@ -6,12 +6,66 @@ import { format } from 'date-fns';
 
 import * as api from '../adapters/ZhawoAdapter';
 
+import SOE from '../assets/img/FloorPlans/SOE';
+
+import TB2 from '../assets/img/FloorPlans/TB/TB2';
+import TB3 from '../assets/img/FloorPlans/TB/TB3';
+import TB4 from '../assets/img/FloorPlans/TB/TB4';
+import TB5 from '../assets/img/FloorPlans/TB/TB5';
+import TB6 from '../assets/img/FloorPlans/TB/TB6';
+
+import TE2 from '../assets/img/FloorPlans/TE/TE2';
+import TE3 from '../assets/img/FloorPlans/TE/TE3';
+import TE4 from '../assets/img/FloorPlans/TE/TE4';
+import TE5 from '../assets/img/FloorPlans/TE/TE5';
+import TE6 from '../assets/img/FloorPlans/TE/TE6';
+
+import TL2 from '../assets/img/FloorPlans/TL/TL2';
+import TL3 from '../assets/img/FloorPlans/TL/TL3';
+import TL4 from '../assets/img/FloorPlans/TL/TL4';
+
+import TH2 from '../assets/img/FloorPlans/TH/TH2';
+import TH3 from '../assets/img/FloorPlans/TH/TH3';
+import TH4 from '../assets/img/FloorPlans/TH/TH4';
+import TH5 from '../assets/img/FloorPlans/TH/TH5';
+
+import TP2 from '../assets/img/FloorPlans/TP/TP2';
+import TP4 from '../assets/img/FloorPlans/TP/TP4';
+
 class RoomSearchStore extends EventEmitter {
   constructor() {
     super();
     this.currentTimeSlot = '';
     this.freeRooms = [];
-    this.currentfreeRooms = [];
+    (this.currentFloor = 'SOE'), (this.currentfreeRooms = []);
+    this.currentFloors = [];
+    this.floors = {
+      SOE: SOE,
+
+      TB2: TB2,
+      TB3: TB3,
+      TB4: TB4,
+      TB5: TB5,
+      TB6: TB6,
+
+      TE2: TE2,
+      TE3: TE3,
+      TE4: TE4,
+      TE5: TE5,
+      TE6: TE6,
+
+      TH2: TH2,
+      TH3: TH3,
+      TH4: TH4,
+      TH5: TH5,
+
+      TL2: TL2,
+      TL3: TL3,
+      TL4: TL4,
+
+      TP2: TP2,
+      TP4: TP4
+    };
   }
 
   async handleActions(action) {
@@ -30,12 +84,15 @@ class RoomSearchStore extends EventEmitter {
         this.currentfreeRooms = this.getSortedByTimeSlot(this.currentTimeSlot);
         this.emit('got_currentFreeRooms');
         break;
+      case 'CHANGE_FLOOR':
+        this.setFloor(action.payload);
+        this.emit('newFloor');
+        break;
     }
   }
 
   getSortedByTimeSlot(value) {
     // Find timeslot
-
     if (value) {
       let found = false;
       let count = 0;
@@ -54,6 +111,25 @@ class RoomSearchStore extends EventEmitter {
     }
 
     return [];
+  }
+
+  setFloor(value) {
+    let nextFloor = value;
+    let tempFloors = [];
+
+    if (nextFloor != 'SOE') {
+      // gets building of selected floor
+      let building = nextFloor.substring(0, 2);
+
+      // gets all floors of building
+      for (let tempFloor in this.floors) {
+        if (tempFloor.substring(0, 2) === building) {
+          tempFloors.push(tempFloor);
+        }
+      }
+    }
+    this.currentFloor = nextFloor;
+    this.currentFloors = tempFloors;
   }
 }
 
