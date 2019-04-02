03acfc0ac73b75af34d116c6838f81af54779e11
bachmdo <bachdomi1@gmail.com>
2019-04-02 12:27:52 +0200

    Added more mocks

--- source/frontend/app/containers/ScheduleContainer/ScheduleContainer.test.js
+++ source/frontend/app/containers/ScheduleContainer/ScheduleContainer.test.js
@@ -1,3 +1,8 @@
+jest.mock('../../stores/GlobalStore');
+jest.mock('../../stores/ScheduleStore');
+jest.mock('../../adapters/IdbAdapter');
+jest.mock('../../adapters/ZhawoAdapter');
+
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, shallow } from 'enzyme';
@@ -8,21 +13,16 @@ import globalStore from '../../stores/GlobalStore';
 
 import ScheduleContainer from './ScheduleContainer';
 
-const wrapper = shallow(<ScheduleContainer />);
-const instance = wrapper.instance();
-
-jest.mock('../../stores/GlobalStore');
-jest.mock('../../actions/GlobalActions');
-jest.mock('../../stores/ScheduleStore');
-jest.mock('../../actions/ScheduleActions');
-
 it('renders without crashing', () => {});
 
 it('should render one root element with className ScheduleContainer', () => {
+  const wrapper = shallow(<ScheduleContainer />);
   expect(wrapper.find('.ScheduleContainer')).toHaveLength(1);
 });
 
 it('should call setState with the correct value via toggleMonthView', () => {
+  const wrapper = shallow(<ScheduleContainer />);
+  const instance = wrapper.instance();
   instance.state.isOpen = true;
   instance.getFreeRoomsByTime = jest.fn();
   instance.setState = jest.fn();
@@ -32,6 +32,8 @@ it('should call setState with the correct value via toggleMonthView', () => {
 });
 
 it('should call setState with the correct value via handleView', () => {
+  const wrapper = shallow(<ScheduleContainer />);
+  const instance = wrapper.instance();
   globalStore.isDayView = true;
   instance.getFreeRoomsByTime = jest.fn();
   instance.setState = jest.fn();
