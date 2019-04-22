e647a6ff3b6892a8e949a33c1a19642829eee974
bachmdo <bachdomi1@gmail.com>
2019-04-22 19:48:17 +0200

    Added backend endpoints/logic to get next vsZhawEvents

--- source/backend/app/adapters/VszhawAdapter.js
+++ source/backend/app/adapters/VszhawAdapter.js
@@ -1,4 +1,7 @@
 import Parser from 'rss-parser';
+import fetch from 'node-fetch';
+import { format, addMonths } from 'date-fns';
+import ical from 'node-ical';
 
 export const parser = new Parser();
 
@@ -6,5 +9,36 @@ export async function getVszhawRSS() {
   return new Promise(async (resolve, reject) => {
     let feed = await parser.parseURL('https://www.vszhaw.ch/feed/');
     resolve(feed.items);
+  });
+}
+
+export async function getVszhawEvents() {
+  return new Promise(async (resolve, reject) => {
+    let foundAnEvent = false;
+    let monthsAhead = 0;
+    let date = new Date();
+    let events = [];
+    while (!foundAnEvent && monthsAhead < 2) {
+      date = addMonths(date, monthsAhead);
+      let dateString = format(date, 'YYYY-MM');
+      let url = `https://www.vszhaw.ch/events/${dateString}/?ical=1`;
+      let response = await fetch(url);
+      let icalString = await response.text();
+      let icalData = ical.parseICS(icalString);
+      Object.keys(icalData).forEach(key => {
+        const entry = icalData[key];
+        if (entry.type == 'VEVENT') {
+          let event = {
+            eventName: entry.summary,
+            eventDate: new Date(entry.start),
+            eventUrl: entry.url
+          };
+          events.push(event);
+        }
+      });
+      foundAnEvent = events.length > 0;
+      monthsAhead++;
+    }
+    resolve(events);
   });
 }
