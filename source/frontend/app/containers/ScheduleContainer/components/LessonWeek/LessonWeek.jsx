1e9789b2c4f5d56a125e767dccc80d5d76efce64
bachmdo <bachdomi1@gmail.com>
2019-04-22 17:42:58 +0200

    Added indicator for overlapping events in WeekView, added activeSlot in WeekView

--- source/frontend/app/containers/ScheduleContainer/components/LessonWeek/LessonWeek.jsx
+++ source/frontend/app/containers/ScheduleContainer/components/LessonWeek/LessonWeek.jsx
@@ -13,6 +13,7 @@ import EventDetailDialog from '../EventDetailDialog/EventDetailDialog';
 
 class LessonWeek extends Component {
   state = {
+    activeSlot: '',
     slots: scheduleStore.slots,
     displayDay: scheduleStore.displayDay,
     schedule: scheduleStore.schedule,
@@ -26,6 +27,16 @@ class LessonWeek extends Component {
 
   componentWillUnmount() {
     scheduleStore.removeListener('schedule_changed', this.refreshSchedule);
+    this.stopTimer();
+  }
+
+  componentDidMount() {
+    // Check every x seconds if active slot is still active
+    this.startTimer();
+    // Set initial active slot once
+    this.setState({
+      activeSlot: this.getTimeSlot(this.state.slots)
+    });
   }
 
   refreshSchedule = () => {
@@ -36,77 +47,126 @@ class LessonWeek extends Component {
     });
   };
 
+  // returns the current TimeSlot
+  getTimeSlot = slots => {
+    const now = format(new Date(), 'HH:mm');
+    for (var slot in slots) {
+      if (format(slots[slot].endTime, 'HH:mm') > now) {
+        return format(slots[slot].endTime, 'HH:mm');
+      }
+    }
+    return null;
+  };
+
+  startTimer() {
+    if (!this.timerId) {
+      this.timerId = setInterval(() => {
+        this.setState({
+          activeSlot: this.getTimeSlot(this.state.slots)
+        });
+      }, 60000);
+    }
+  }
+
+  stopTimer() {
+    clearInterval(this.timerId);
+  }
+
   handleEventClick = param => e => {
-    this.setState({ eventDetailsOpen: true, eventForDetails: param });
+    // Check if it is event with actual information (not. f.ex. holiday)
+    if (param.eventRealizations && param.eventRealizations.length > 0) {
+      this.setState({ eventDetailsOpen: true, eventForDetails: param });
+    }
   };
 
   handleCloseEventDetails = () => {
     this.setState({ eventDetailsOpen: false, eventForDetails: null });
   };
 
+  getWeekKey = displayDay => {
+    return format(startOfWeek(displayDay, { weekStartsOn: 1 }), 'YYYY-MM-DD');
+  };
+
   render() {
-    const weekKey = format(
-      startOfWeek(this.state.displayDay, { weekStartsOn: 1 }),
-      'YYYY-MM-DD'
-    );
-    const dayKey = format(this.state.displayDay, 'YYYY-MM-DD');
-    const isThereData =
-      this.state.schedule !== null &&
-      this.state.schedule.weeks !== undefined &&
-      this.state.schedule.weeks[weekKey] !== undefined;
+    const {
+      slots,
+      displayDay,
+      schedule,
+      eventDetailsOpen,
+      eventForDetails,
+      activeSlot
+    } = this.state;
+    const weekKey = this.getWeekKey(displayDay);
     return (
       <Fragment>
-        {this.state.eventDetailsOpen && (
+        {eventDetailsOpen && (
           <EventDetailDialog
             open={true}
-            event={this.state.eventForDetails}
+            event={eventForDetails}
             handleClose={this.handleCloseEventDetails}
           />
         )}
-        {this.state.slots.map(slot => (
-          <Fragment key={format(slot.startTime, 'HH:mm')}>
-            <div className="SlotTime">
-              <div className="SlotStartTime">
-                {format(slot.startTime, 'HH:mm')}
-              </div>
-              <div className="SlotEndTime">{format(slot.endTime, 'HH:mm')}</div>
-            </div>
+        {slots.map((slot, index) => (
+          <Fragment key={index}>
+            <Slots
+              activeSlot={activeSlot}
+              startTime={slot.startTime}
+              endTime={slot.endTime}
+            />
           </Fragment>
         ))}
-        {isThereData &&
-          Object.entries(this.state.schedule.weeks[weekKey]).map(
-            ([key, day, dayNr]) =>
-              day.slots.map(
-                (slot, i) =>
-                  slot.events &&
-                  slot.events.map((event, j) => (
-                    <Fragment
-                      key={format(slot.startTime, 'HH:mm').concat(event.name)}
-                    >
-                      <div
-                        className="LessonWeekEvent"
-                        onClick={this.handleEventClick(event)}
-                        style={{
-                          gridColumnStart: 1 + getDay(new Date(key)),
-                          gridRow: `${i + 1} / ${i + 1 + event.slots.length}`
-                        }}
+        {schedule &&
+          schedule.weeks &&
+          schedule.weeks[weekKey] &&
+          Object.entries(schedule.weeks[weekKey]).map(([key, day, dayNr]) =>
+            day.slots.map(
+              (slot, i) =>
+                slot.eventBucket &&
+                slot.eventBucket.map((event, j) => {
+                  if (j === 0) {
+                    return (
+                      <Fragment
+                        key={format(slot.startTime, 'HH:mm').concat(event.name)}
                       >
-                        <div className="EventInfo">{event.name}</div>
-                        <br />
-                        <div className="EventRoom">
-                          {event.eventRealizations &&
-                            event.eventRealizations[0] &&
-                            event.eventRealizations[0].room &&
-                            event.eventRealizations[0].room.name}
+                        <div
+                          className="LessonWeekEvent"
+                          onClick={this.handleEventClick(event)}
+                          style={{
+                            gridColumnStart: 1 + getDay(new Date(key)),
+                            gridRow: `${i + 1} / ${i + 1 + event.slots.length}`
+                          }}
+                        >
+                          <div className="EventInfo">{event.name}</div>
+                          <br />
+                          <div className="EventRoom">
+                            {event.eventRealizations &&
+                              event.eventRealizations[0] &&
+                              event.eventRealizations[0].room &&
+                              event.eventRealizations[0].room.name}
+                          </div>
+                          <br />
+                          {slot.eventBucket.length > 1 && (
+                            <div className="EventMore">
+                              {slot.eventBucket.length} Events
+                            </div>
+                          )}
                         </div>
-                      </div>
-                    </Fragment>
-                  ))
-              )
+                      </Fragment>
+                    );
+                  }
+                })
+            )
           )}
       </Fragment>
     );
   }
 }
+
+const Slots = ({ activeSlot, startTime, endTime }) => (
+  <div className={'SlotTime ' + (activeSlot == format(endTime, 'HH:mm'))}>
+    <div className="SlotStartTime">{format(startTime, 'HH:mm')}</div>
+    <div className="SlotEndTime">{format(endTime, 'HH:mm')}</div>
+  </div>
+);
 
 export default LessonWeek;
