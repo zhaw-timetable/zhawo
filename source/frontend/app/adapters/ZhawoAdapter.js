02c24a5760fff28e19187e752e88daffb52c4963
vissejul <vissejul@students.zhaw.ch>
2019-04-25 13:46:33 +0200

    made deployable to surge

--- source/frontend/app/adapters/ZhawoAdapter.js
+++ source/frontend/app/adapters/ZhawoAdapter.js
@@ -12,7 +12,8 @@ const GET = 'GET';
 const POST = 'POST';
 const HEADERS = { 'Content-Type': 'application/json' };
 let address = 'http://localhost:4000';
-if (process.env.NODE_ENV === 'production') address = window.location.origin;
+if (process.env.NODE_ENV === 'production')
+  address = 'http://160.85.252.131:4000';
 const apiUrl = `${address}/api/v1`;
 
 export function getMensaResource(facilityId, date) {
