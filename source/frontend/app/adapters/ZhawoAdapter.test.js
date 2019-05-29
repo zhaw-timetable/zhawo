7e64d5eb14669cbf7a7f5acd2a1805b643e2d971
bachmdo <bachdomi1@gmail.com>
2019-05-29 11:46:54 +0200

    Tested all functions in ZhawoAdapter

--- source/frontend/app/adapters/ZhawoAdapter.test.js
+++ source/frontend/app/adapters/ZhawoAdapter.test.js
@@ -1,3 +1,5 @@
+import { format, addDays } from 'date-fns';
+
 import * as api from './ZhawoAdapter';
 
 beforeEach(() => {
@@ -129,6 +131,180 @@ it('getPossibleNames should call correct api endpoints and concatenate responses
   expect(response.classes).toBeDefined();
   expect(response.courses).toBeDefined();
   expect(response.rooms).toBeDefined();
+  expect(response.students.length).toBe(2);
+  expect(response.lecturers.length).toBe(2);
+  expect(response.classes.length).toBe(2);
+  expect(response.courses.length).toBe(2);
+  expect(response.rooms.length).toBe(2);
+  expect(response.students[0].label).toEqual('student1');
+  expect(response.lecturers[0].label).toEqual('lecturer1');
+  expect(response.classes[0].label).toEqual('class1');
+  expect(response.courses[0].label).toEqual('course1');
+  expect(response.rooms[0].label).toEqual('room1');
 });
 
-// fetch.mockResponses([JSON.stringify(FETCH_RESPONSE), JSON.stringify(FETCH_RESPONSE),JSON.stringify(FETCH_RESPONSE),JSON.stringify(FETCH_RESPONSE),JSON.stringify(FETCH_RESPONSE)]);
+it('resolveOverlaps should return newSchedule with eventBuckets', () => {
+  const INPUT_SCHEDULE = {
+    weeks: {
+      week1: {
+        day1: {
+          slots: [
+            {
+              testId: 'slot1',
+              events: [
+                {
+                  testId: 'event1',
+                  slots: [{ testId: 'slot1' }, { testId: 'slot2' }]
+                }
+              ]
+            },
+            {
+              testId: 'slot2',
+              events: [
+                {
+                  testId: 'event2',
+                  slots: [{ testId: 'slot2' }]
+                }
+              ]
+            },
+            {
+              testId: 'slot3',
+              events: [
+                {
+                  testId: 'event3',
+                  slots: [{ testId: 'slot3' }]
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  };
+  const newSchedule = api.resolveOverlaps(INPUT_SCHEDULE);
+  // should sort in event2 into bucket with first event since they overlap.
+  // event3 should be in its own bucket
+  const slots = newSchedule.weeks.week1.day1.slots;
+  expect(slots[0].eventBucket.length).toBe(2);
+  expect(slots[1].eventBucket.length).toBe(0);
+  expect(slots[2].eventBucket.length).toBe(1);
+  expect(slots[0].eventBucket[0].testId).toEqual('event1');
+  expect(slots[0].eventBucket[0].offSetFromBucketStart).toBe(0);
+  expect(slots[0].eventBucket[1].testId).toEqual('event2');
+  expect(slots[0].eventBucket[1].offSetFromBucketStart).toBe(1);
+  expect(slots[2].eventBucket[0].testId).toEqual('event3');
+});
+
+it('convertSchedule should reorganize schedule into weeks object with date keys and days with date keys', () => {
+  const FIXED_DATE = new Date('2019-05-29T08:00:00+01:00');
+  const DAY_KEY = format(FIXED_DATE, 'YYYY-MM-DD');
+  const WEEK_KEY = format(FIXED_DATE, 'YYYY-MM-DD');
+  const INPUT_SCHEDULE = {
+    days: [
+      {
+        date: FIXED_DATE,
+        slots: DEFAULT_SLOTS,
+        events: [
+          {
+            testId: 'event1',
+            slots: [
+              {
+                endTime: '2018-10-29T08:45:00+01:00',
+                startTime: '2018-10-29T08:00:00+01:00'
+              },
+              {
+                endTime: '2018-10-29T09:35:00+01:00',
+                startTime: '2018-10-29T08:50:00+01:00'
+              }
+            ]
+          },
+          {
+            testId: 'event2',
+            slots: [
+              {
+                endTime: '2018-10-29T10:45:00+01:00',
+                startTime: '2018-10-29T10:00:00+01:00'
+              },
+              {
+                endTime: '2018-10-29T11:35:00+01:00',
+                startTime: '2018-10-29T10:50:00+01:00'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  const newSchedule = api.convertSchedule(INPUT_SCHEDULE);
+  expect(newSchedule.weeks[WEEK_KEY]).toBeDefined();
+  expect(newSchedule.weeks[WEEK_KEY][DAY_KEY]).toBeDefined();
+  expect(
+    newSchedule.weeks[WEEK_KEY][DAY_KEY].slots[0].events[0].testId
+  ).toEqual('event1');
+  expect(
+    newSchedule.weeks[WEEK_KEY][DAY_KEY].slots[2].events[0].testId
+  ).toEqual('event2');
+});
+
+const DEFAULT_SLOTS = [
+  {
+    endTime: '2018-10-29T08:45:00+01:00',
+    startTime: '2018-10-29T08:00:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T09:35:00+01:00',
+    startTime: '2018-10-29T08:50:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T10:45:00+01:00',
+    startTime: '2018-10-29T10:00:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T11:35:00+01:00',
+    startTime: '2018-10-29T10:50:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T12:45:00+01:00',
+    startTime: '2018-10-29T12:00:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T13:35:00+01:00',
+    startTime: '2018-10-29T12:50:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T14:45:00+01:00',
+    startTime: '2018-10-29T14:00:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T15:35:00+01:00',
+    startTime: '2018-10-29T14:50:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T16:45:00+01:00',
+    startTime: '2018-10-29T16:00:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T17:35:00+01:00',
+    startTime: '2018-10-29T16:50:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T18:30:00+01:00',
+    startTime: '2018-10-29T17:45:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T19:25:00+01:00',
+    startTime: '2018-10-29T18:40:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T20:10:00+01:00',
+    startTime: '2018-10-29T19:25:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T21:05:00+01:00',
+    startTime: '2018-10-29T20:20:00+01:00'
+  },
+  {
+    endTime: '2018-10-29T21:50:00+01:00',
+    startTime: '2018-10-29T21:05:00+01:00'
+  }
+];
